a31a72426203bf95999e837460cb328559509695
johnb9682 <johnb9682@gmail.com>
2016-09-19 15:14:17 +0800

    Initial commit

---
+++ src/main/java/com/solexange/App.java
@@ -0,0 +1,13 @@
+package com.solexange;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
